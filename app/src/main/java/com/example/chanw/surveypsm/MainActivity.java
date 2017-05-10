1115a718395ca85bb3146ac4ceaa6ecfcba61aaa
Wilson Chan <Wilson Chan>
2017-05-10 20:06:45 +0800

    uncommented OnPause for refresh listView

--- app/src/main/java/com/example/chanw/surveypsm/MainActivity.java
+++ app/src/main/java/com/example/chanw/surveypsm/MainActivity.java
@@ -95,9 +95,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adapter.notifyDataSetChanged();
+    }
+//
 //    @Override
-//    protected void onResume() {
-//        super.onResume();
+//    protected void onPause() {
+//        super.onPause();
 //        adapter.notifyDataSetChanged();
 //    }
 
